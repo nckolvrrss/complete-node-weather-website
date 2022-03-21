b5713d48a65e99ad1f97e8f57fe4fcb05d988b5e
Nicholas Ross <nicholas.ross@commit.dev>
2022-03-21 12:29:09 -0300

    changes for heroku deploy

--- public/js/app.js
+++ public/js/app.js
@@ -10,16 +10,14 @@ weatherForm.addEventListener('submit', (e) => {
   messageOne.textContent = 'Loading...'
   messageTwo.textContent = ''
 
-  http: fetch('http://127.0.0.1:3000/weather?address=' + location).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          messageOne.textContent = data.error
-        } else {
-          messageOne.textContent = data.location
-          messageTwo.textContent = data.forecast
-        }
-      })
-    },
-  )
+  http: fetch('/weather?address=' + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error
+      } else {
+        messageOne.textContent = data.location
+        messageTwo.textContent = data.forecast
+      }
+    })
+  })
 })
